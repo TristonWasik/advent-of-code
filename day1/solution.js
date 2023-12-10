a83721530719c6b59a5b69114adf75992be36ea8
Triston Wasik <twasik4@gmail.com>
2023-12-09 20:22:33 -0700

    part 2 WIP

--- day1/solution.js
+++ day1/solution.js
@@ -1,25 +1,76 @@
 const fs = require("fs");
 
 let values = [];
+let debugArr = [];
 
 /**
  * Read file, add calibration to array
  */
-readCalibration = async () => {
+readCalibration = async (debug = false) => {
   const contents = fs.readFileSync(__dirname + "/calibration.txt", "utf-8");
 
-  values = contents.split(/\r?\n/).map((line) => {
-    const numArr = line.match(/\d/g);
-    return parseInt(numArr[0] + numArr.at(-1));
+  values = contents.split(/\r?\n/).map((line, i) => {
+    const numArr = line.match(
+      /\d|(?:one|two|three|four|five|six|seven|eight|nine|zero)/g
+    );
+
+    if (debug) {
+      console.log(
+        numArr,
+        convertToNumberString(numArr[0]) + convertToNumberString(numArr.at(-1))
+      );
+      debugArr.push(
+        `${i}:\t${line}\t[${numArr.toString().replaceAll(",", ".")}]\t\t\t${
+          convertToNumberString(numArr[0]) +
+          convertToNumberString(numArr.at(-1))
+        }`
+      );
+    }
+
+    return parseInt(
+      convertToNumberString(numArr[0]) + convertToNumberString(numArr.at(-1))
+    );
   });
 
+  fs.writeFileSync(
+    __dirname + "/debug.txt",
+    debugArr.toString().replaceAll(",", "\n")
+  );
+
   console.log(`Parsed file, added ${values.length} calibrations.`);
+};
+
+convertToNumberString = (value) => {
+  switch (value) {
+    case "one":
+      return "1";
+    case "two":
+      return "2";
+    case "three":
+      return "3";
+    case "four":
+      return "4";
+    case "five":
+      return "5";
+    case "six":
+      return "6";
+    case "seven":
+      return "7";
+    case "eight":
+      return "8";
+    case "nine":
+      return "9";
+    case "zero":
+      return "0";
+    default:
+      return value;
+  }
 };
 
 calculateTotal = (values) => values.reduce((a, b) => a + b);
 
 main = () => {
-  readCalibration();
+  readCalibration(true);
   console.log(calculateTotal(values));
 };
 
