8e1c691cd38d21073438eeb88b141bc97c9c7cf7
Triston Wasik <twasik4@gmail.com>
2023-12-13 16:53:03 -0700

    finish day2 part2

--- src/solutions/day2.ts
+++ src/solutions/day2.ts
@@ -15,9 +15,18 @@ const processFile = () => {
     key: string;
     count: number;
     isPossible: boolean;
+    results: {
+      red: number;
+      green: number;
+      blue: number;
+    };
   }[] = [];
 
   const processedGames = games.map((game: string, i: number) => {
+    const blues = getNumberArrayFromStringByRegex(game, /\d+\sblue/g);
+    const reds = getNumberArrayFromStringByRegex(game, /\d+\sred/g);
+    const greens = getNumberArrayFromStringByRegex(game, /\d+\sgreen/g);
+
     hands = game.match(/\d+\s\w+/g)!.map((set: string) => {
       const [count, key] = set.split(/\s/);
 
@@ -28,15 +37,33 @@ const processFile = () => {
           ? true
           : false;
 
-      console.log(`${i + 1}: [${key}] ${count} - ${isPossible}`);
+      // console.log(`${i + 1}: [${key}] ${count} - ${isPossible}`);
 
-      return { game: i + 1, key, count: +count, isPossible };
+      return {
+        game: i + 1,
+        key,
+        count: +count,
+        isPossible,
+        results: {
+          red: reds[0],
+          green: greens[0],
+          blue: blues[0],
+        },
+      };
     });
 
     return {
       game: i + 1,
-      hands,
+      hands: hands.map(
+        (hand) => `${hand.key}: ${hand.count} | ${hand.isPossible}`
+      ),
       isPossible: hands.every((s) => s.isPossible === true),
+      highest: {
+        red: reds[0],
+        green: greens[0],
+        blue: blues[0],
+      },
+      result: reds[0] * greens[0] * blues[0],
     };
   });
 
@@ -49,8 +76,26 @@ const processFile = () => {
   console.table(processedGames);
 
   // output final result
-  console.log(`Total: ${possibleGames.reduce((a, b) => a + b, 0)}`);
+  console.log(
+    `Total: ${possibleGames.reduce(
+      (a, b) => a + b,
+      0
+    )}; Sum of set powers: ${processedGames
+      .map((g) => g.result)
+      .reduce((a, b) => a + b, 0)}`
+  );
 
   // sanity check total possible
   // console.log([...Array(100)].map((_, i) => ++i).reduce((a, b) => a + b, 0));
+};
+
+const getNumberArrayFromStringByRegex = (input: string, regex: RegExp) => {
+  return input
+    .match(regex)!
+    .map((b) => {
+      const [count, key] = b.split(/\s/);
+
+      return +count;
+    })
+    .sort((a, b) => (a > b ? -1 : 1));
 };
